e07ad8dab2faf0ba20317625cfd5d1e222a5c625
Amie Croteau <amiecroteau@gmail.com>
2018-08-21 20:15:51 -0400

    Corrected Readme and made several test changes

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,18 +27,18 @@ $(function () {
 		});
 
 
-		/* TODO: Write a test that loops through each feed
+		/* test that loops through each feed
 		 * in the allFeeds object and ensures it has a URL defined
 		 * and that the URL is not empty.
 		 */
-		it('url defined', function () {
+		it('url length defined', function () {
 			for (let feed of allFeeds) {
-				expect(feed.url).toBeDefined();
-				expect(feed.url).not.toBe(0);
+				expect(feed.url.length).toBeDefined();
+				expect(feed.url.length).not.toBe(0);
 			}
 		});
 
-		/* TODO: Write a test that loops through each feed
+		/*  test that loops through each feed
 		 * in the allFeeds object and ensures it has a name defined
 		 * and that the name is not empty.
 		 */
@@ -49,9 +49,9 @@ $(function () {
 			}
 		});
 	});
-	/* TODO: Write a new test suite named "The menu" */
+	/*  test suite named "The menu" */
 	describe('The menu', function () {
-		/* TODO: Write a test that ensures the menu element is
+		/*  test that ensures the menu element is
 		 * hidden by default. You'll have to analyze the HTML and
 		 * the CSS to determine how we're performing the
 		 * hiding/showing of the menu element.
@@ -62,37 +62,32 @@ $(function () {
 			expect(body.classList.contains('menu-hidden')).toBe(true);
 
 		});
-		/* TODO: Write a test that ensures the menu changes
+		/* test that ensures the menu changes
 		 * visibility when the menu icon is clicked. This test
 		 * should have two expectations: does the menu display when
 		 * clicked and does it hide when clicked again.
 		 */
 
-		it('menu toggles on', function () {
+		it('menu toggles on and off', function () {
 			const body = document.querySelector('body');
 			const menu = document.querySelector('.menu-icon-link');
+			
 			menu.click();
-			body.classList.remove('menu-hidden');
 			expect(body.classList.contains('menu-hidden')).toBe(false);
-
-		});
-
-
-		it('menu toggles off', function () {
-			const body = document.querySelector('body');
-			const menu = document.querySelector('.menu-icon-link');
 			menu.click();
-			body.classList.add('menu-hidden');
 			expect(body.classList.contains('menu-hidden')).toBe(true);
 
 		});
 
+
+		
+
 	});
 
-	/* TODO: Write a new test suite named "Initial Entries" */
+	/*Write a new test suite named "Initial Entries" */
 	
 	describe('Initial Entries', function(){
-		/* TODO: Write a test that ensures when the loadFeed
+		/*Write a test that ensures when the loadFeed
 	 * function is called and completes its work, there is at least
 	 * a single .entry element within the .feed container.
 	 * Remember, loadFeed() is asynchronous so this test will require
@@ -111,9 +106,9 @@ $(function () {
 		});
 					
 	});
-/* TODO: Write a new test suite named "New Feed Selection" */
+/*Write a new test suite named "New Feed Selection" */
 	describe ('New Feed Selection', function(){
-/* TODO: Write a test that ensures when a new feed is loaded
+/*  Write a test that ensures when a new feed is loaded
 	 * by the loadFeed function that the content actually changes.
 	 * Remember, loadFeed() is asynchronous.
 	 */
